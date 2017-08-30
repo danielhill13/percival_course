7f3aa7469dde6e0e085f8e99631cfc1fb8ddfd92
Daniel Hill <daniel.hill13@gmail.com>
2017-08-30 16:12:42 -0700

    working on weather app

---
+++ weatherapp/weather.php
@@ -0,0 +1,31 @@
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="weather.css">
+</head>
+<body>
+<div id='bg'>
+
+    <div class='jumbotron'>
+        <h1 class=''>What's The Weather?</h1>
+        <p>Enter the name of a city</p>
+        <form method='POST'>
+            <div class='form-group'>
+                <label for="city"></label>
+                <input class="form-control"type="text" name='city' placeholder='city'>
+
+            </div>
+            <input type="submit" class='btn btn-primary'>
+        </form>
+    </div>
+
+<?php
+//Take input and query http://www.weather-forecast.com/ with the text search
+
+//Return the conditions in a box below the form
+
+?>
+
+
+
+</div>
+</body>
